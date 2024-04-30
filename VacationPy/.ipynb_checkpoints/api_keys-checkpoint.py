963e94c5878c1375818172684e8a82d841bc5f09
chrigabr0628 <christina.gabriel@eastern.edu>
2024-04-29 23:42:29 -0500

    final update

---
+++ VacationPy/.ipynb_checkpoints/api_keys-checkpoint.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "bfc917cb43b100710f9d3bd84b11e60c"
+
+# Geoapify API Key
+geoapify_key = "YOUR KEY HERE"
---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "bfc917cb43b100710f9d3bd84b11e60c"
+
+# Geoapify API Key
+geoapify_key = "ec0d2372e6fb495da452830fe551e27a"
